551023b9365226f802b3fd1146446795f3df06f4
Toresp <mikillterminator@gmail.com>
2021-03-15 11:53:11 +0100

    En el calendario los datos se escriben correctamente en el archivo .json

--- app/src/main/java/com/example/proyectomiguelsambademartnez/Calendario.java
+++ app/src/main/java/com/example/proyectomiguelsambademartnez/Calendario.java
@@ -2,32 +2,24 @@ package com.example.proyectomiguelsambademartnez;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.gson.JsonArray;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 
 public class Calendario extends AppCompatActivity {
     private WebView calendario;
-    private List<PassData> contraseñas;
+    private List<PassData> contrasenhas;
     private calendarPassData calendarData;
 
     @SuppressLint("JavascriptInterface")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendario);
-        contraseñas = ((UserData) getIntent().getSerializableExtra(sesion_iniciada.DATOS)).getContraseñas();
-        calendarData = new calendarPassData(contraseñas);
+        contrasenhas = ((UserData) getIntent().getSerializableExtra(sesion_iniciada.DATOS)).getContraseñas();
+        calendarData = new calendarPassData(contrasenhas);
         calendarData.writeJson(this.getApplicationContext());
         calendario = (WebView) this.findViewById(R.id.calendar);
         calendario.getSettings().setJavaScriptEnabled(true);
--- app/src/main/java/com/example/proyectomiguelsambademartnez/calendarPassData.java
+++ app/src/main/java/com/example/proyectomiguelsambademartnez/calendarPassData.java
@@ -1,7 +1,6 @@
 package com.example.proyectomiguelsambademartnez;
 
 import android.content.Context;
-import android.webkit.JavascriptInterface;
 
 import com.google.gson.JsonArray;
 
@@ -11,8 +10,6 @@ import org.json.JSONObject;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
-import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -20,30 +17,37 @@ import java.util.List;
 public class calendarPassData{
     private HashMap<String, String> events = new HashMap<>();
     private List<Integer> posDif = new ArrayList<>();
-    private List<PassData> contraseñas;
+    private List<PassData> contrasenhas;
 
-    calendarPassData(List<PassData> Pass){
-        this.contraseñas = Pass;
+    calendarPassData(List<PassData> Pass) {
+        this.contrasenhas = Pass;
         String date;
         List<String> e = new ArrayList<>();
-        for (int i = 0;i<Pass.size(); i++){
+        /*Se recorre la lista para juntar todas los eventos con fechas iguales en un mismo String
+        y así enviarlo luego en el JSON*/
+        for (int i = 0; i < Pass.size(); i++) {
             e.add(Pass.get(i).getPage());
-            int j = i+1;
-            if(j<Pass.size()) {
+            int j = i + 1;
+            if (j < Pass.size()) {
+                try {
 
+                    while (Pass.get(i).getCreation_date().equals(Pass.get(j).getCreation_date())) {
+                        e.add(Pass.get(j).getPage());
+                        j++;
+                    }
+                } catch (Exception Ex) {
 
-                while (Pass.get(i).getCreation_date().equals(Pass.get(j).getCreation_date())) {
-                    e.add(Pass.get(j).getPage());
-                    j++;
                 }
+                date = "";
+                for (String s : e) {
+                    date += s + ", " ;
+                }
+                    events.put(contrasenhas.get(i).getCreation_date(), date);
+
+                posDif.add(i);
+                e.clear();
+                i = j - 1;
             }
-            date = "";
-            for (String s : e) {
-                date += ", " + s;
-            }
-            events.put(e.get(i),date);
-            posDif.add(i);
-            i=j-1;
         }
     }
 
@@ -52,7 +56,7 @@ public class calendarPassData{
         for ( int i=0;i<posDif.size();i++){
             JSONObject data = new JSONObject();
             try {
-                String date=contraseñas.get(posDif.get(i)).getCreation_date();
+                String date= contrasenhas.get(posDif.get(i)).getCreation_date();
                 data.put("date", date);
                 data.put("event","Cambiar contraseña de la pagina "+ events.get(date));
                 Datos.add(String.valueOf(data));
